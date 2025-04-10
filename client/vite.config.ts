02b85ce2372262c4c1e375ea245ae3fa982f4e5a
Joel Erlandsson <erlandssonjoel@gmail.com>
2025-04-10 13:43:18 +0200

    vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -11,17 +11,20 @@ export default defineConfig({
       "@shared/types": path.resolve(__dirname, "../shared/types"),
     },
   },
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      external: ["zod"],
+    },
+  },
   server: {
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:5001", // Din Express-server
+        target: "http://localhost:5001",
         changeOrigin: true,
         secure: false,
       },
     },
-  },
-  build: {
-    outDir: "dist",
   },
 });
