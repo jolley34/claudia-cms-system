bb3771b840c46d623bb11f246ce451d5df1d6750
Joel Erlandsson <erlandssonjoel@gmail.com>
2025-04-10 16:32:14 +0200

    changed viteconfig

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,11 @@
 // client/vite.config.ts
 import react from "@vitejs/plugin-react";
-import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      "@shared/schemas": path.resolve(__dirname, "../shared/schemas"),
-      "@shared/types": path.resolve(__dirname, "../shared/types"),
-    },
+    alias: {},
   },
   build: {
     outDir: "dist",
