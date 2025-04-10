494837cadb5368d0286f7cae3c5c5e25a90eb4e9
Joel Erlandsson <erlandssonjoel@gmail.com>
2025-04-10 18:19:32 +0200

    updated cors

--- server/src/config/cors.ts
+++ server/src/config/cors.ts
@@ -1,8 +1,21 @@
 import cors from "cors";
 
 export const corsOptions = {
-  origin: "http://localhost:3000", // Anpassa efter frontend-url
-  credentials: true, // Tillåt cookies och autentisering
+  origin: (
+    origin: string | undefined,
+    callback: (err: Error | null, allow?: boolean) => void
+  ) => {
+    const allowedOrigins = [
+      "http://localhost:3000",
+      "https://claudia-cms-system.netlify.app",
+    ];
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
